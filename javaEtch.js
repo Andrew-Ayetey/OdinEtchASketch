c3897987145511377fde7b2df6a47cf49f748397
Andrew Ayetey <ayeteya@gmail.com>
2019-11-07 12:29:30 -0600

    created etch box

---
+++ javaEtch.js
@@ -0,0 +1,21 @@
+let createDiv = function(){
+    let etchDiv = document.createElement('div')
+    let etchBox = document.getElementById('Etch-a-Sketch')
+    etchDiv.classList.add("etchBlock")
+    etchBox.appendChild(etchDiv)
+
+}
+
+let makeDivs = function(number){
+    totalBlocks = number ** 2
+    for(let i = 0; i < totalBlocks; i++){
+        createDiv()
+    }
+    for(i = 0; i < number; i++){
+        let etchBox = document.getElementById('Etch-a-Sketch')
+        etchBox.style.gridTemplateColumns += " 1fr"
+        etchBox.style.gridTemplateRows += " 1fr"
+    }
+}
+
+makeDivs(50)
