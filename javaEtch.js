c650299046be080ba902b239b4a7d5f1c0b7f54e
Andrew Ayetey <ayeteya@gmail.com>
2019-11-15 15:40:41 -0600

    added color button

--- javaEtch.js
+++ javaEtch.js
@@ -2,12 +2,10 @@
 let createDiv = function(){
     let etchDiv = document.createElement('div')
     let etchBox = document.getElementById('Etch-a-Sketch')
-    // console.log(typeof etchBox.firstChild)
     etchDiv.classList.add("etchBlock")
     etchDiv.style.background = 'rgba(0,0,0)'
     etchDiv.style.opacity = '0'
     etchBox.appendChild(etchDiv)
-    console.log(typeof etchBox.firstChild)
     return
 
 }
@@ -28,8 +26,6 @@ let makeDivsStart = function(number){
     return makeDivs(number)
 }
 
-
-
 makeDivsStart(4)
 
 let blockDivsNode = document.querySelectorAll(".etchBlock")
@@ -38,15 +34,21 @@ let blockLength = blockDivsArray.length
 
 let clearDivs = function(){
     let etchBox = document.getElementById('Etch-a-Sketch')
-    console.log(blockLength)
     for(let i = 0; i < blockLength; i++){
             let firstKid = etchBox.firstChild
-            console.log(typeof firstKid)
             etchBox.removeChild(firstKid)
     }
     
     etchBox.style.gridTemplateColumns = ""
     etchBox.style.gridTemplateRows = ""
+}
+
+let randomColor = function(){
+    r = Math.floor(Math.random() * 255)
+    g = Math.floor(Math.random() * 255)
+    b = Math.floor(Math.random() * 255)
+    return "rgba("+ r + "," + g + "," + b +")"
+
 }
 
 //event listeners
@@ -63,22 +65,33 @@ let addEtchEvent = function(){
     }
 }
 
+let resetDivFunction = function(){
+    let resetDivs = prompt("Please enter a new number between 2 and 100", "")
+    if(Number(resetDivs) >= 2 || Number(resetDivs) >= 100){
+        clearDivs()
+        makeDivs((Number(resetDivs)))
+        blockDivsNode = document.querySelectorAll(".etchBlock")
+        blockDivsArray = Array.from(blockDivsNode)
+        blockLength = blockDivsArray.length
+        addEtchEvent()
+    }
+    else{resetDivFunction()}
+}   
+
+
 let resetButton = document.getElementById('reset')
 resetButton.addEventListener('click', e => {
-    let resetDivFunction = function(){
-        let resetDivs = prompt("Please enter a new number between 2 and 100", "")
-        if(Number(resetDivs) >= 2 || Number(resetDivs) >= 100){
-            clearDivs()
-            makeDivs((Number(resetDivs)))
-            blockDivsNode = document.querySelectorAll(".etchBlock")
-            blockDivsArray = Array.from(blockDivsNode)
-            blockLength = blockDivsArray.length
-            addEtchEvent()
-        }
-        else{resetDivFunction()}   
-    }
     resetDivFunction()
+})
 
-    })
+let colorButton = document.getElementById('colorReset')
+colorButton.addEventListener('click', e => {
+    resetDivFunction()
+    for(const divs in blockDivsArray){
+        console.log(blockDivsArray[divs])
+        blockDivsArray[divs].style.background = randomColor()
+    }
+    
+})
 
 addEtchEvent()
